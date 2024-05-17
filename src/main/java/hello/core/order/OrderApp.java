39f6c11b50ff982657135a8101e691c0cca0b289
memoryDev <memorydev2024@gmail.com>
2024-05-17 15:15:48 +0900

    3. 스프링 핵심원리 이해1 - 스프링 컨테이너와 스프링 빈

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -1,14 +1,21 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         //1. 멤버 생성(메모리에 저장하고있어서 실행시마다 멤버추가)
         Long memberId = 1L;
@@ -16,7 +23,7 @@ public class OrderApp {
         memberService.join(member);
 
         //2. 주문생성
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
 
